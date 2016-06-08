adcfea3b2b63dfdae220c5be417a639fcc223667
Sidney <user@SGD-MBP.local>
2016-06-07 16:45:47 -1000

    2nd Day 3rd Work

--- secret_number.js
+++ secret_number.js
@@ -1,4 +1,20 @@
 'use strict';
 module.exports = function() {
-  
-};
+    
+
+
+    var storedNum = 0;
+    var keepNum = storedNum;
+
+    var secretNumberGenerator = (function(){
+     storedNum = Math.random() * 1000000;
+     return storedNum;
+    });
+
+    return secretNumberGenerator;
+
+
+
+
+
+}; //This closes dakine.

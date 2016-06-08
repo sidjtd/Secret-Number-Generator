2bf06bbdd099b54c7e11599ce20c4fca5b8ebf52
Sidney <user@SGD-MBP.local>
2016-06-07 18:10:38 -1000

    2nd Day 3rd Work

--- secret_number.js
+++ secret_number.js
@@ -1,14 +1,38 @@
 'use strict';
 module.exports = function() {
     
+       var secretNumber = Math.floor((Math.random() * 1000000)+1);
+       
+       return function(){
+        return secretNumber;
+       };
+};
 
 
-    var storedNum = 0;
-    var keepNum = storedNum;
+
+ //This closes dakine.
+
+
+/*'use strict';
+module.exports = function() {
+    
+
+
+    var secretNumber = 0;
+    var originalNumber = 0;
+    var emptyArray = [];
 
     var secretNumberGenerator = (function(){
-     storedNum = Math.random() * 1000000;
-     return storedNum;
+
+     if (emptyArray[0]===secretNumber){
+
+     }else{
+        secretNumber = Math.floor((Math.random() * 1000000)+1);
+        emptyArray.push = secretNumber;
+        return secretNumber;  
+     }
+
+    
     });
 
     return secretNumberGenerator;
@@ -18,3 +42,26 @@ module.exports = function() {
 
 
 }; //This closes dakine.
+*/
+
+/*'use strict';
+module.exports = function() {
+    
+
+
+    var secretNumber = 0;
+    var keepTrack = true;
+    var originalNumber = 0;
+
+    var secretNumberGenerator = (function(){
+
+     //if (keepTrack){
+         secretNumber = Math.floor(Math.random() * 1000000);
+         originalNumber = secretNumber;
+         keepTrack = false;
+         return secretNumber;   
+     //}
+    });
+
+    return secretNumberGenerator;
+}; //This closes dakine.*/
